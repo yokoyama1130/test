ff2bbc86335d92e3e5d0d74761297388ab066255
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-05 18:18:24 +0900

    テキストウィジェットの出力

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 
 // flutterのmain関数
 void main() {
-  const a = MaterialApp(
+  final col = Column(children: const [
+    Text('レモン'),
+    Text('りんご'),
+    Text('ブドウ'),
+  ]);
+
+  final a = MaterialApp(
               home: Scaffold(
                 body: Center(
-                  child: Text('バナナ'),
+                  child: col,
                 ),
               ),
             );
