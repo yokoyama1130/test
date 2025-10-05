6be3493ed14b3f55c8723639d6b77eef1c85fa73
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-10-05 18:05:18 +0900

    省略して階段状にする書き方に修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,12 @@ import 'package:flutter/material.dart';
 
 // flutterのmain関数
 void main() {
-  // ここにコードを書く
-
-  // バナナをbという変数にする
-  // 適当な文字を変数にしているだけ
-  const b = 'バナナ';
-
-  // bという変数をtというウィジェットにする
-  // textっていうウィジェット
-  // flutterではテキストもウィジェットに変換して使う
-  const t = Text(b);
-
-  // tというウィjヴェットをCというウィジェットに変換
-  // centerというウィジェットはウィジェットを真ん中に置くためのウィジェット
-  const c = Center(child: t);
-
-  // cというウィジェットをsというウィジェットにする
-  // Scaffoldは「はしご」という意味
-  // 一つの画面を作りたい時使う
-  const s = Scaffold(body: c);
-
-  // sというウィジェットをaというウィエットにする
-  // MaterialAppは作ったものをアプリにするウィジェット
-  const a = MaterialApp(home: s);
-
-  // 最後にこれで完成
-  // runで動かす
+  const a = MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('バナナ'),
+                ),
+              ),
+            );
   runApp(a);
 }
